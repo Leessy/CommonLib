9613e93732deb9b7ade39c9e210c393b23c5a632
LiuC <355994698@qq.com>
2018-07-04 15:32:09 +0800

    aiface

--- app/src/main/java/com/leessy/liuc/commonlib/MainActivity.java
+++ app/src/main/java/com/leessy/liuc/commonlib/MainActivity.java
@@ -1,18 +1,44 @@
 package com.leessy.liuc.commonlib;
 
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
+import com.kaer.sdk.IDCardItem;
+import com.kaer.sdk.serial.SerialReadClient;
+import com.leessy.liuc.commonlibs.TestOBS;
 import com.leessy.liuc.commonlibs.TimeUilts;
 
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    SerialReadClient seri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TimeUilts.showDialog(this, "hello!!");
-        new AlertDialog.Builder(this).setTitle("???").setMessage(TimeUilts.getNowDate()).show();
+//        TimeUilts.showDialog(this, "hello!!");
+//        new AlertDialog.Builder(this).setTitle("???").setMessage(TimeUilts.getNowDate()).show();
+        Observable<Integer> getobs = TestOBS.getobs();
+        getobs.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.d(TAG, "accept: =" + integer);
+            }
+        });
+
+        seri = TimeUilts.getSeri();
+        int i = seri.openSerialPort("/dev/ttySAC0", 115200);
+        Log.d(TAG, "onCreate: 卡尔启动=" + i);
+
+    }
+
+    public void test(View view) {
+        IDCardItem item = seri.readCertWithoutNet();//本地
+        Log.d(TAG, "test: 读卡结果=" + item.toString());
     }
 }
